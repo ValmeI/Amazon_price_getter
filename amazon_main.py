2323a85d522b1e324bad0d822405138091446960
Ignar <Ignarvalme@gmail.com>
2023-06-18 18:37:37 +0300

    nr update

--- amazon_main.py
+++ amazon_main.py
@@ -71,8 +71,8 @@ if __name__ == '__main__':
 
     TARGET_PRICE = 280
 
-    # amazon_product_code_list = ['B0BDK2CZ8N', 'B0BDJ3ND5X', 'B0BDJ55SSD']  # Pixel 7 Pro
-    amazon_product_code_list = ['B0BDJG3TWP', 'B0BDK63RF3', 'B0BDJFKY7B'] # Pixel 7
+    amazon_product_code_list = ['B0BDK2CZ8N', 'B0BDJ3ND5X', 'B0BDJ55SSD']  # Pixel 7 Pro
+    # amazon_product_code_list = ['B0BDJG3TWP', 'B0BDK63RF3', 'B0BDJFKY7B'] # Pixel 7
     # amazon_product_code_list = ['B08C7KG5LP', 'B091CQH6VT', 'B08C7KCJF5']  # Sony WH-1000XM4 big ones
     # amazon_product_code_list = ['B095D1HCYG', 'B095DNPH4R'] # Sony WF-1000XM4 Buds
     # Create a queue to hold the results
